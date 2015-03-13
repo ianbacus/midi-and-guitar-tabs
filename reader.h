6e1a764a956db6d7a8e73160ad23e61330b5accb
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 17:18:53 -0400

    Update reader.h

--- reader.h
+++ reader.h
@@ -23,7 +23,7 @@ public:
 	
 	void read_header_chunk();
 	void read_track_chunk();
-	void read_varlen();
+	unsigned int read_varlen();
 	void read_bytes_to_char(ifstream &bin,int range);
 	void read_bytes_to_int(ifstream &bin, int range,unsigned int &ret);
 	void discard_bytes(int range);
