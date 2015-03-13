b47a43312d1cf9a5bce874e4795213345ec5aa23
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 14:42:02 -0400

    Create note.h

---
+++ note.h
@@ -0,0 +1,16 @@
+//implement as doubly linked list
+class Chunk : public Base 
+{
+	public:
+		int delta;
+		vector<Note*> Chunk_Notes;
+	
+		void add_Note(Note*); //this should automatically enforce good fingering with the tuning matrix
+		void rotate();
+		vector<Note*> * get_chunk_notes_vector() {return &Chunk_Notes;}
+		
+		virtual void reconfigure(Base*);
+		virtual bool compare_chunks(Chunk*);
+	
+	
+};
