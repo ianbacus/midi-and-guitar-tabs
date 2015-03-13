2222cf51ec2e9b6c52ac7fe7bb2f357c0d951c8c
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 18:02:15 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,42 @@
 #include "reader.h"
 
+
+void read_chunk(Reader &ro)
+{
+  
+	string type;
+	string mtrk = "MTrk";
+	string mthd = "MThd";
+	
+	ro.read_bytes_to_char(4,type);
+	cout << type << endl;
+//	if(type.compare(mthd) == 0)
+	if(type == "MThd")
+	{
+	cout << "reading header chunk" << endl;
+		ro.read_header_chunk();
+	}
+//	else if(type.compare(mtrk) == 0)
+	else if(type == "MTrk")
+	{
+	cout << "reading track chunk" << endl;
+		ro.read_track_chunk();
+	}
+	else
+	{
+	  cout << "fuckup" << endl;
+	}
+
+}
+
+
 void read_file(Reader &ro)
 {
 	unsigned int track_cnt = 0;
-	ro.read_chunk();
+	read_chunk(ro);
 	while(track_cnt < 2) //ro.get_tracks())
 	{
-		ro.read_chunk();
+		read_chunk(ro);
 		track_cnt++;
 	}
 }
