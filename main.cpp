7915238237824f1ea6e1eb3c944d9e0f4e6c0a8f
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 17:40:42 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "reader.h"
 
+void read_file(Reader &ro)
+{
+	unsigned int track_cnt = 0;
+	read_chunk(ro);
+	while(track_cnt < 2) //ro.get_tracks())
+	{
+		read_chunk(ro);
+		track_cnt++;
+	}
+}
 
 int main()
 {
   string fn ("canon.mid");
   Reader robj(fn);
- // read_file(robj);
+  read_file(robj);
   return 0;
 }
