97b55535ac71d4adbe64f45f33ce078ded2a4aa6
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 17:14:27 -0400

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -93,14 +93,15 @@ void Reader::generate_map()
 
 Reader::Reader (string &fn)
 {
-    int index = 0,
+	int index = 0,
   	header = 0,
   	format=0,
   	tracks=0,
   	time_div=0,
   	time_sig=0, 
-  	tempo=0,
-	filename = fn;
+  	tempo=0;
+  	
+  string filename = fn;
  // generate_map();
   
     ifstream temp(fn, ios::binary );
