eb6be6ae549d1292b15ed54d9425c791bcfeca69
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 16:16:01 -0400

    Create RotateVIsitor.cpp

---
+++ RotateVIsitor.cpp
@@ -0,0 +1,21 @@
+#include "Visitor.h"
+
+//This will recursively check and reconfigure all of the elements of the tree
+
+
+RotateVisitor::visitBar() 
+{
+  
+}
+
+
+RotateVisitor::visitChunk() 
+{
+  
+}
+
+
+RotateVisitor::visitNote() 
+{
+  
+}
