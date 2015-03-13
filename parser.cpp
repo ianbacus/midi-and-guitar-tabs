7a1309690382499e15ce828ece3d0bb06ef46a7b
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 17:17:47 -0400

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -93,13 +93,13 @@ void Reader::generate_map()
 
 Reader::Reader (string &fn)
 {
-	int index = 0,
-  	header = 0,
-  	format=0,
-  	tracks=0,
-  	time_div=0,
-  	time_sig=0, 
-  	tempo=0;
+  index = 0;
+  header = 0;
+  format=0;
+  tracks=0;
+  time_div=0;
+  time_sig=0; 
+  tempo=0;
   	
   string filename = fn;
  // generate_map();
