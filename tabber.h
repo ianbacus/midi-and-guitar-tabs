0fefeb4c1ad3b83df596374d7d94d84aac806ca6
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 15:32:39 -0400

    Update tabber.h

--- tabber.h
+++ tabber.h
@@ -34,8 +34,7 @@ class Base
 		//accessors
 		
 		//retrieve string and fret information from the fret map with modulo addressing (circular vector)
-		vector< pair<int,int> * >  > get_vector(int i) {//return pitch_to_frets_map[i];
-		return pitch_to_frets_map.find(i);}
+		vector< pair<int,int> * >  > get_vector(int i) {return pitch_to_frets_map.find(i);}
 		int get_string(int,int) const; 
 		int get_fret(int,int) const;
 		int get_pitch_to_frets_entry_size(int pitch) const;
