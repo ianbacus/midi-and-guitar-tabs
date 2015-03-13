a72ccc178e50299fcf695df9bb159a1e021cc42e
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 18:08:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ void read_chunk(Reader &ro)
 	
 	ro.read_bytes_to_char(4,type);
 	cout << type << endl;
-//	if(type.compare(mthd) == 0)
-	if(type == "MThd")
+	if(type.compare(mthd) == 0)
+//	if(type == "MThd")
 	{
 	cout << "reading header chunk" << endl;
 		ro.read_header_chunk();
 	}
-//	else if(type.compare(mtrk) == 0)
-	else if(type == "MTrk")
+	else if(type.compare(mtrk) == 0)
+//	else if(type == "MTrk")
 	{
 	cout << "reading track chunk" << endl;
 		ro.read_track_chunk();
