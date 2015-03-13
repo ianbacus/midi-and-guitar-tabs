3ca5a09fe85f17ee6ff78f157e99b4f14175eebd
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 16:06:52 -0400

    Create RotateVisitor.h

---
+++ RotateVisitor.h
@@ -0,0 +1,17 @@
+#ifndef __ROTATEVISITOR__
+#define __ROTATEVISITOR__
+
+#include "visitor.h"
+
+
+class RotateVisitor : public Visitor
+{
+
+  public:
+    visitNote();
+    visitBar();
+    visitChunk();
+
+};
+
+#endif
