5591265b81a736f542ee0c8533ca5a9e315a0c17
ianbacus <ianbacus@users.noreply.github.com>
2015-03-14 14:37:06 -0400

    Update bar.h

--- bar.h
+++ bar.h
@@ -10,13 +10,14 @@ class Bar : public Base
 	// - absorb this class into Chunk, treat that like a nonterminal node
 	
 	public:
-		vector<Chunk*> Bar_Chunks; //Chunks are children of Bar	
+		vector<Chunk*> _bar_chunks; //Chunks are children of Bar	
 
 		void add_Chunk(Chunk*);
 		//operator [] for accessing Chunks
 		virtual void reconfigure();
 		
 		virtual void accept(Visitor*v) {v->visitBar(this);}
+		virtual int get_children_size() {return _bar_chunks.size();}
 };
 
 #endif
