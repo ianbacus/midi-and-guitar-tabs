e61503da39fb610a8666e987cca646843ecdcb65
ianbacus <ianbacus@users.noreply.github.com>
2015-03-14 11:42:53 -0400

    Update chunk.h

--- chunk.h
+++ chunk.h
@@ -9,11 +9,14 @@ class Chunk : public Base
 {
 	public:
 		int delta;
-		vector<Note*> Chunk_Notes;
+		vector<Note*> _chunk_notes;
 	
 		void add_Note(Note*); //this should automatically enforce good fingering with the tuning matrix
 		void rotate();
-		vector<Note*> * get_chunk_notes_vector() {return &Chunk_Notes;}
+		
+		Note* get_note_at(int i) {return _chunk_notes[i];}
+		int get_chunk_size() {return _chunk_notes.size();}
+		//vector<Note*> * get_chunk_notes_vector() {return &_chunk_notes;}
 		
 		virtual void accept(Visitor* v) {v.visitChunk(this);}
 	//	virtual void reconfigure(); //removed parameter Base*
