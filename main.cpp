48ac5a5524ea48c1728892756c1ebdbd88bb118e
ianbacus <ianbacus@users.noreply.github.com>
2015-03-14 15:00:14 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,19 +11,12 @@
 
 typedef unsigned char uchar;
 
-// user interface variables
+
 Options options;
 int     debugQ = 0;             // use with --debug option
 int     maxcount = 100000;
 double  tempo = 60.0;
 
-// function declarations:
-void convertMidiFileToText (MidiFile& midifile);
-void setTempo (MidiFile& midifile, int index, double& tempo);
-void checkOptions (Options& opts, int argc, char** argv);
-void example (void);
-void usage (const char* command);
-//////////////////////////////////////////////////////////////////////////
 
 using namespace std;
 struct Note {
