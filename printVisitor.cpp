b25f1ee2adc818574d28a6d35fa18b98cd7d49b2
ianbacus <ianbacus@users.noreply.github.com>
2015-03-14 14:02:13 -0400

    Update printVisitor.cpp

--- printVisitor.cpp
+++ printVisitor.cpp
@@ -1,12 +1,42 @@
 #include "printvisitor.h"
 
+void printVisitor::visitBar(Bar* b)
+{
+
+  for(string_print_index=1; string_print_index<6; string_print_index++)
+  {
+    if(string_print_index==0)
+    {
+      string_buffer += " ";
+    }
+    else
+    {
+      string_buffer += "|";
+    }
+    for(int j=0; j<b->get_children_size(); j++)
+    {
+      b->get_child(j)->accept(this);
+    }
+    //TODO: add logic to this to make the number of continuous bar prints variable by the client
+    string_buffer += "\n";
+  }
+}
+
+
+void printVisitor::visitChunk(Chunk* c)
+{
+  for(int j=0; j<c->get_children_size(); j++)
+  {
+    Note* current_note = get_note_at(j);
+    if(current_note->get_string() == string_print_index)
+    {
+      current_note->accept(this);
+    }
+  }
+  
+}
 
 void printVisitor::visitNote(Note* n)
 {
-}
-void printVisitor::visitBar(Bar* b)
-{
-}
-void printVisitor::visitChunk(Chunk* c)
-{
+  string_buffer += n->get_fret();
 }
