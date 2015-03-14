0b92ce2e4a2578064442309e56020c626512ca3e
ianbacus <ianbacus@users.noreply.github.com>
2015-03-13 21:21:13 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 #include <iostream>
 
 using namespace std;
+struct note {
+      double tick;
+      double duration;
+      int    pitch;
+};
+
 /*
 void read_chunk(Reader &ro)
 {
@@ -44,6 +50,61 @@ void read_file(Reader &ro)
 		track_cnt++;
 	}
 }*/
+//////////////////////////////
+//
+// convertToMelody --
+//
+
+void convertToMelody(MidiFile& midifile, vector<Note>& melody) {
+   midifile.absoluteTicks();
+   if (track < 0 || track >= midifile.getNumTracks()) {
+      cout << "Invalid track: " << track << " Maximum track is: "
+           << midifile.getNumTracks() - 1 << endl;
+   }
+   int numEvents = midifile.getNumEvents(track);
+
+   vector<int> state(128);   // for keeping track of the note states
+
+   int i;
+   for (i=0; i<128; i++) {
+      state[i] = -1;
+   }
+
+   melody.reserve(numEvents);
+   melody.clear();
+
+   Melody mtemp;
+   int command;
+   int pitch;
+   int velocity;
+
+   for (i=0; i<numEvents; i++) {
+      command = midifile[track][i][0] & 0xf0;
+      if (command == 0x90) {
+         pitch = midifile[track][i][1];
+         velocity = midifile[track][i][2];
+         if (velocity == 0) {
+            // note off
+            goto noteoff;
+         } else {
+            // note on
+            state[pitch] = midifile[track][i].tick;
+         }
+      } else if (command == 0x80) {
+         // note off
+         pitch = midifile[track][i][1];
+noteoff:
+         if (state[pitch] == -1) {
+            continue;
+         }
+         mtemp.tick = state[pitch];
+         mtemp.duration = midifile[track][i].tick - state[pitch];
+         mtemp.pitch = pitch;
+         melody.push_back(mtemp);
+         state[pitch] = -1;
+      }
+   }
+}
 
 int main()
 {
