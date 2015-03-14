c94c38477e6f54f302d40fa32fde22b4ac6a9dc2
ianbacus <ianbacus@users.noreply.github.com>
2015-03-14 16:59:20 -0400

    Update Base.cpp

--- Base.cpp
+++ Base.cpp
@@ -27,6 +27,7 @@ void Base::grid_init()
 
 int Base::get_string(int pitch,int index) const
 {
+	cout << pitch << " is on string " << (pitch_to_frets_map.find(pitch)->second)[index]->first;
 	return (pitch_to_frets_map.find(pitch)->second)[index]->first;
 	
 }
@@ -35,6 +36,7 @@ int Base::get_fret(int pitch, int index) const
 {
 	//const vector<pair<int,int>* > pairy = (pitch_to_frets_map.find(pitch)->second);
 	//cout << pairy[index]->second << endl;
+	cout << pitch " is on fret " << (pitch_to_frets_map.find(pitch)->second)[index]->second;
 	return (pitch_to_frets_map.find(pitch)->second)[index]->second;
 }
 
@@ -43,6 +45,7 @@ int Base::get_pitch_to_frets_entry_size(int pitch) const
 //returns the size of a vector of tab matrix coordinates associated with a pitch. Equivalently, returns the number
 // of frettable locations for a note.
 {
+	cout << "entry is " << (pitch_to_frets_map.find(pitch)->second).size() " units long" << endl;
 	return (pitch_to_frets_map.find(pitch)->second).size();
 
 }
