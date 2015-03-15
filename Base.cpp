5f4a28a658f98950dff7db6c8de4bf943145043b
ianbacus <ianbacus@users.noreply.github.com>
2015-03-15 01:56:15 -0400

    Update Base.cpp

--- Base.cpp
+++ Base.cpp
@@ -1,6 +1,6 @@
 #include "base.h"
 
-void Base_structs::config()
+void Base::config()
 {
     int value;
     tuning[0] = 28;  //E
@@ -24,10 +24,10 @@ void Base_structs::config()
     }
 }
 
-/*
+
 int Base::get_string(int pitch,int index) const
 {
-	cout << pitch << " is on string " << ((pitch_to_frets_map.find(pitch)->second)[index]->first) << endl;
+//	cout << pitch << " is on string " << ((pitch_to_frets_map.find(pitch)->second)[index]->first) << endl;
 	return (pitch_to_frets_map.find(pitch)->second)[index]->first;
 	
 }
@@ -36,7 +36,7 @@ int Base::get_fret(int pitch, int index) const
 {
 	//const vector<pair<int,int>* > pairy = (pitch_to_frets_map.find(pitch)->second);
 	//cout << pairy[index]->second << endl;
-	cout << pitch << " is on fret " << ((pitch_to_frets_map.find(pitch)->second)[index]->second) << endl;
+//	cout << pitch << " is on fret " << ((pitch_to_frets_map.find(pitch)->second)[index]->second) << endl;
 	return (pitch_to_frets_map.find(pitch)->second)[index]->second;
 }
 
@@ -45,7 +45,7 @@ int Base::get_pitch_to_frets_entry_size(int pitch) const
 //returns the size of a vector of tab matrix coordinates associated with a pitch. Equivalently, returns the number
 // of frettable locations for a note.
 {
-	cout << "entry is " << (pitch_to_frets_map.find(pitch)->second).size() << " units long" << endl;
+//	cout << "entry is " << (pitch_to_frets_map.find(pitch)->second).size() << " units long" << endl;
 	return (pitch_to_frets_map.find(pitch)->second).size();
 
-}*/
+}
