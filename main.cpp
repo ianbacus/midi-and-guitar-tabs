b5d90ef45587156305cbf2b88ee5b0ede12d224a
ianbacus <ianbacus@users.noreply.github.com>
2015-03-15 02:10:29 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,32 @@
 
 
 using namespace std;
+void config()
+{
+int tuning[6];
+//int tab_matrix [6][20];
+    int value;
+    tuning[0] = 28;  //E
+    tuning[1] = 33; //A
+    tuning[2] = 39; //D
+    tuning[3] = 43; //G
+    tuning[4] = 47; //B
+    tuning[5] = 52; //E
+     for(int string_ind = 0;string_ind<5;string_ind++)
+    {
+        for(int fret_ind = 0; fret_ind<20; fret_ind++)
+        {
+            {
+                value = tuning[string_ind] + fret_ind;
+                //cout << "gridmap: inserting " << value << " at coordinate point " << string_ind <<", " << fret_ind << endl;
+                //tab_matrix[string_ind][fret_ind] = value;
+                pair<int,int> map_point = make_pair(string_ind,fret_ind);
+                Base::pitch_to_frets_map[value].push_back(&map_point); //add note to (note : location on fretboard) pitch_to_frets. this will help for determining how many placements there are for a note, and quickly indexing them (is this any faster than indexing the array? TODO)
+            }
+        }
+    }
+}
+
 /*
 struct Note {
       double tick;
