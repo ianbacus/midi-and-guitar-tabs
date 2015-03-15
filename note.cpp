7b23ffa7e498e742cf66b814ab3e0351c2bb6189
ianbacus <ianbacus@users.noreply.github.com>
2015-03-15 12:38:54 -0400

    Update note.cpp

--- note.cpp
+++ note.cpp
@@ -14,14 +14,14 @@ int Note::get_string() const
 {
 	//return pitch_to_frets_map[pitch][current_note_index]->second;
 //	return Base_structs::(pitch_to_frets_map.find(pitch)->second)[index]->first;
-cout << "Note::" << pitch << " " << get_string(pitch,current_note_index) << endl;
-	return Base::get_string(pitch,current_note_index);
+cout << "Note::" << pitch << " " << get_string_pos(pitch,current_note_index) << endl;
+	return Base::get_string_pos(pitch,current_note_index);
 }
 
 int Note::get_fret() const
 {
 	//return pitch_to_frets_map[pitch][current_note_index]->first;
-	return Base::get_fret(pitch,current_note_index);
+	return Base::get_fret_pos(pitch,current_note_index);
 //	return Base_structs::(pitch_to_frets_map.find(pitch)->second)[index]->second;
 	
 }
