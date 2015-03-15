271baa7af41fdb199465441eaf217d1b8a01cc98
ianbacus <ianbacus@users.noreply.github.com>
2015-03-15 12:53:40 -0400

    Update note.cpp

--- note.cpp
+++ note.cpp
@@ -2,7 +2,37 @@
 #define ACCEPTABLE  8
 //Note should have a vector of possible string positions (which depend on the tuning in the tab matrix) by default
 // hold an index to tabmatrix, update by incrementing 
-// 
+//
+
+Note::PitchMap Note::config()
+{
+    Base::PitchMap initmap;
+    int value;
+    int tuning[6];
+    tuning[0] = 28;  //E
+    tuning[1] = 33; //A
+    tuning[2] = 39; //D
+    tuning[3] = 43; //G
+    tuning[4] = 47; //B
+    tuning[5] = 52; //E
+     for(int string_ind = 0;string_ind<6;string_ind++)
+    {
+        for(int fret_ind = 0; fret_ind<20; fret_ind++)
+        {
+            {
+                value = tuning[string_ind] + fret_ind;
+                cout << "gridmap: inserting " << value << " at coordinate point " << string_ind <<", " << fret_ind << endl;
+                //tab_matrix[string_ind][fret_ind] = value;
+                pair<int,int> map_point = make_pair(string_ind,fret_ind);
+                initmap[value].push_back(&map_point); //add note to (note : location on fretboard) pitch_to_frets. this will help for determining how many placements there are for a note, and quickly indexing them (is this any faster than indexing the array? TODO)
+            	cout << value <<  "first/string " << (pitch_to_frets_map.at(value))[0]->first;
+            	cout << "second/fret "<< (pitch_to_frets_map.at(value))[0]->second << endl;
+            	
+            }
+        }
+    }
+    return initmap;
+}
 Note::Note(int pitch_init)
 {
 	pitch = pitch_init;
@@ -12,18 +42,28 @@ Note::Note(int pitch_init)
 
 int Note::get_string() const
 {
-	//return pitch_to_frets_map[pitch][current_note_index]->second;
-//	return Base_structs::(pitch_to_frets_map.find(pitch)->second)[index]->first;
-//cout << "Note::" << pitch << " " << get_string_pos(pitch,current_note_index) << endl;
-	return get_string_pos(pitch,current_note_index);
+	int ret;
+	try {
+		ret = (pitch_to_frets_map.at(pitch))[index]->first;
+	}
+	catch (const std::out_of_range& oor) {
+		std::cerr << "Out of Range error: " << oor.what() << '\n';
+		//cout << "uh oh";
+	}
+	return ret;
 }
 
 int Note::get_fret() const
 {
-	//return pitch_to_frets_map[pitch][current_note_index]->first;
-	return get_fret_pos(pitch,current_note_index);
-//	return Base_structs::(pitch_to_frets_map.find(pitch)->second)[index]->second;
-	
+	int ret;
+	try {
+		ret = (pitch_to_frets_map.at(pitch))[index]->second;
+	}
+	catch (const std::out_of_range& oor) {
+		std::cerr << "Out of Range error: " << oor.what() << '\n';
+		//cout << "uh oh";
+	}
+	return ret;	
 }
 
 void Note::increment_note_index()
@@ -56,3 +96,7 @@ bool Note::compare(Note* note) const
 		}
 	}
 	*/
+	
+	
+Note::PitchMap Note::pitch_to_frets_map = Note::config();
+
