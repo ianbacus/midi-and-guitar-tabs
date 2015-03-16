aae2d4a4f8e61057ef0e078e7f80d5d7756e3c13
ianbacus <ianbacus@users.noreply.github.com>
2015-03-15 23:44:46 -0400

    Update printVisitor.cpp

--- printVisitor.cpp
+++ printVisitor.cpp
@@ -44,8 +44,9 @@ void PrintVisitor::visitChunk(Chunk* c)
 
 void PrintVisitor::visitNote(Note* n)
 {
-  cout << n->get_fret() << endl;
-  string_buffer[string_print_index] += n->get_fret();
+  int fret = n->get_fret();
+  cout << fret << endl;
+  string_buffer[string_print_index] += fret;
   cout << " string so far: ." << string_buffer[string_print_index] <<"." << endl;
   
 }
